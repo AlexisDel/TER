be116baca7a005761255d54b26e62dda2c32d49a
AlexisDel <AlexisDel@users.noreply.github.com>
2023-04-17 16:53:41 +0200

    comparator (for the result part)

--- comparator.py
+++ comparator.py
@@ -1,13 +1,21 @@
+import copy
+
 filename = "d4-lactose-H2O2"
-
-f = open(filename + ".ssa", "r")
 f_correct = open(filename + "_patrick.ssa", "r")
+f = open(filename + ".ssa", "r")
 
+p_result = len(f_correct.readlines()) - 2
+print("Number of reactions (patrick's file) :", p_result)
+print("Number of reactions (our file) :", len(f.readlines()) - 2)
+
+f_correct = open(filename + "_patrick.ssa", "r")
+counter = 0
 for line_correct in f_correct.readlines():
+    f = open(filename + ".ssa", "r")
     for line in f.readlines():
         if line == line_correct:
-            print("OK")
-        else:
-            print(line)
-            print(line_correct)
-            print("-------------")
+            counter += 1
+    f.close()
+
+print("Number of common reactions :", counter - 2)
+print("Recouvrement :", float(counter - 2) / float(p_result) * 100, "%")
